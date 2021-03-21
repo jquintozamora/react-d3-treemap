0bde14537e1a980ef1ce74584ac04d617dfee5d7
jquintozamora <quintorel@hotmail.com>
2021-03-21 16:41:38 +0000

    update webpacl

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -7,14 +7,14 @@ const protocol = process.env.HTTPS === "true" ? "https" : "http";
 const host = process.env.HOST || "localhost";
 
 module.exports = {
-  // To enhance the debugging process. More info: https://webpack.js.org/configuration/devtool/
+  mode: "development",
   devtool: "inline-source-map",
   target: "web",
   entry: {
     "react.d3.treemap": "./src/index.tsx"
   },
   output: {
-    filename: "static/js/[name].[hash].js",
+    filename: "static/js/[name].[fullhash].js",
     path: commonPaths.outputPath,
     pathinfo: true,
     publicPath: "./"
@@ -24,7 +24,7 @@ module.exports = {
   },
   devServer: {
     hot: true,
-    contentBase: commonPaths.contentBasePath, // match the output path
+    contentBase: commonPaths.contentBasePath, 
     publicPath: "/",
     host: host,
     https: protocol === "https",
@@ -32,22 +32,21 @@ module.exports = {
     disableHostCheck: true,
     historyApiFallback: true,
     stats: {
-      colors: true, // color is life
-      chunks: false, // this reduces the amount of stuff I see in my terminal; configure to your needs
+      colors: true, 
+      chunks: false, 
       "errors-only": true
     }
   },
+  optimization: {
+    moduleIds: "named"
+  },
   plugins: [
-    // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "./index.html",
       inject: 'body'
     }),
-    // enable HMR globally
     new webpack.HotModuleReplacementPlugin(),
-    // prints more readable module names in the browser console on HMR updates
-    new webpack.NamedModulesPlugin()
   ],
   module: {
     rules: [
@@ -79,14 +78,6 @@ module.exports = {
           },
           {
             loader: "css-loader",
-            // options: {
-            //   sourceMap: true,
-            //   importLoaders: 1,
-            //   localsConvention: "camelCase",
-            //   modules: {
-            //     localIdentName: "[name]_[local]_[hash:base64:5]"
-            //   }
-            // }
           }
         ]
       }
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,13 +1,9 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////
-//  WebPack 2 PROD Config
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
 const resolve = require("path").resolve;
 const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
+  mode: "production",
   devtool: "source-map",
   entry: {
     "react.d3.treemap": "./src/indexPackage.ts"
@@ -16,16 +12,12 @@ module.exports = {
   output: {
     path: resolve(__dirname, "./../dist"),
     filename: "[name].js",
-    // Possible value - amd, commonjs, commonjs2, commonjs-module, this, var
     libraryTarget: "umd",
-    // library bundle to be available as a global variable when imported
     library: "ReactD3Treemap"
   },
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: [".ts", ".tsx", ".js"]
   },
-  // Exclude React from the bundle, must be react and react-dom here otherwise will not be excluded
   externals: {
     react: {
       root: "React",
@@ -44,55 +36,28 @@ module.exports = {
   },
   optimization: {
     minimize: true,
-    minimizer: [
-      // Plugings for optimizing size and performance.
-      // Here you have all the available by now:
-      //    Webpack 1. https://github.com/webpack/webpack/blob/v1.13.3/lib/optimize
-      //    Webpack 2. https://github.com/webpack/webpack/tree/master/lib/optimize
-      //    Webpack 3. uglify-js is not external (peer) dependency.
-      //              We should install version <= 2.8 by now (19/06/2017) because version 3 is not supported by plugin
-      //    Webpack 4. now uglify comes under optimization
-      new UglifyJSPlugin({
-        sourceMap: true,
-        uglifyOptions: {
-          // https://github.com/mishoo/UglifyJS2/tree/harmony#compress-options
-          compress: {
-            global_defs: {
-              __REACT_HOT_LOADER__: undefined // eslint-disable-line no-undefined
-            }
-          },
-          beautify: false,
-          ecma: 6,
-          comments: false,
-          mangle: false
-        }
-      })
-    ]
   },
   plugins: [
     new webpack.DefinePlugin({
       "process.env": {
-        NODE_ENV: JSON.stringify("production") // Reduces 78 kb on React library
+        NODE_ENV: JSON.stringify("production") 
       },
-      DEBUG: false, // Doesn´t have effect on my example
-      __DEVTOOLS__: false // Doesn´t have effect on my example
+      DEBUG: false, 
+      __DEVTOOLS__: false 
     }),
+    
     new MiniCssExtractPlugin({
       filename: "../dist/[name].css",
-      allChunks: true
     })
   ],
   module: {
-    // loaders -> rules in webpack 2
     rules: [
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       {
         enforce: "pre",
         test: /\.js$/,
         loader: "source-map-loader",
         exclude: ["/node_modules/"]
       },
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       {
         test: /\.ts(x?)$/,
         exclude: /node_modules/,
@@ -101,8 +66,6 @@ module.exports = {
             loader: "ts-loader",
             options: {
               configFile: "tsconfig.package.json"
-              // transpileOnly: true,
-              // logInfoToStdOut: true
             }
           }
         ]
@@ -113,15 +76,7 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader",
-            // options: {
-            //   sourceMap: true,
-            //   importLoaders: 1,
-            //   localsConvention: "camelCase",
-            //   modules: {
-            //     localIdentName: "[name]_[local]_[hash:base64:5]"
-            //   }
-            // }
+            loader: "css-loader"
           }
         ]
       }
